5390ffa32ae9e14c57d2e27fc45ed3b721233622
Christophe <christophe@alsacreations.fr>
2024-09-09 15:32:07 +0200

    First commit

---
+++ projet-init/src/main.ts
@@ -0,0 +1,10 @@
+import './style.scss'
+// import 'virtual:uno.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="square-container">
+  <div class="square">
+   j'ai eu des soucis avec l'install de UnoCss, j'ai commenté la ligne pas trop eu le temps de check pourquoi
+ + mauvais lien sur vite-config.js</div>
+  </div>
+`
